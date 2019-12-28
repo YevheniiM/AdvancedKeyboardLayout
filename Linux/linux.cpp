3cb047f693ddc30e01e9cc8bc21c4808052e2ff2
Nazariy <bachynsky@ucu.edu.ua>
2019-12-28 08:27:22 +0200

    Add OOP
    Implement RAII wrapper for remapping functions

--- Linux/linux.cpp
+++ Linux/linux.cpp
@@ -11,144 +11,33 @@
 #include <X11/XKBlib.h>
 #include <iostream>
 
-#include "keymap.h"
+// #include "keymap.h"
+#include "remapper.hpp"
 
 
 
-void emit(int fd, int type, int code, int val)
-{
-   struct input_event ie;
-
-   ie.type = type;
-   ie.code = code;
-   ie.value = val;
-   /* timestamp values below are ignored */
-   ie.time.tv_sec = 0;
-   ie.time.tv_usec = 0;
-    // std::cout << "sending: " << code << std::endl
-   write(fd, &ie, sizeof(ie));
-}
-
-static const char *const evval[3] = {
-    "RELEASED",
-    "PRESSED ",
-    "REPEATED"
-};
-
-inline void remap (const std::string & original, const std::string & remapped)
-{
-    custom_keymap[std_keymap[original]] = std_keymap[remapped];
-}
-
-void send_new_event(int fdo, int code)
-{
-    if (code != custom_keymap[code])
-    {
-        std::cout << "from: " << code << " to: " << custom_keymap[code] << std::endl;
-        // sleep(1);
-        emit(fdo, EV_KEY, KEY_BACKSPACE, 1);
-        emit(fdo, EV_SYN, SYN_REPORT, 0);
-        emit(fdo, EV_KEY, KEY_BACKSPACE, 0);
-        emit(fdo, EV_SYN, SYN_REPORT, 0);
-        // sleep(1);
-        emit(fdo, EV_KEY, custom_keymap[code], 1);
-        emit(fdo, EV_SYN, SYN_REPORT, 0);
-        emit(fdo, EV_KEY, custom_keymap[code], 0);
-        emit(fdo, EV_SYN, SYN_REPORT, 0);
-        // sleep(1);
-    }
-    else
-    {
-        std::cout << "from: " << code << " to: " << code << std::endl;
-        emit(fdo, EV_KEY, code, 1);
-        emit(fdo, EV_SYN, SYN_REPORT, 0);
-        emit(fdo, EV_KEY, code, 0);
-        emit(fdo, EV_SYN, SYN_REPORT, 0);
-    }
-}
-
-void init_custom_keymap()
-{
-    for (auto & p : std_keymap)
-        custom_keymap[p.second] = p.second;
-}
 
 int main(void)
 {
-   struct uinput_setup usetup;
-    init_custom_keymap();
-   int fdo = open("/dev/uinput", O_WRONLY | O_NONBLOCK);
 
-//    /*
-//     * The ioctls below will enable the device that is about to be
-//     * created, to pass key events, in this case the space key.
-//     */
-   ioctl(fdo, UI_SET_EVBIT, EV_KEY);
-   for (auto & p: std_keymap)
-    //    std::cout << p.second << std::endl;
-      ioctl(fdo, UI_SET_KEYBIT, p.second);
-
-   memset(&usetup, 0, sizeof(usetup));
-   usetup.id.bustype = BUS_USB;
-   usetup.id.vendor = 0x1234; /* sample vendor */
-   usetup.id.product = 0x5678; /* sample product */
-   strcpy(usetup.name, "Example device");
-
-   ioctl(fdo, UI_DEV_SETUP, &usetup);
-   ioctl(fdo, UI_DEV_CREATE);
-   /*
-    * On UI_DEV_CREATE the kernel will create the device node for this
-    * device. We are inserting a pause here so that userspace has time
-    * to detect, initialize the new device, and can start listening to
-    * the event, otherwise it will not notice the event we are about
-    * to send. This pause is only needed in our example code!
-    */
-
+    auto rmp = Remapper();
     // auto custom_keymap = std_keymap;
-    remap("KEY_A", "KEY_S");
-    remap("KEY_S", "KEY_O");
-    remap("KEY_D", "KEY_S");
-    remap("KEY_F", "KEY_I");
-    // for (auto & k: custom_keymap)
-    // {
-        // std::cout << k.first << " : " << k.second << std::endl;
-    // }
-    int fdi;
-    const char * dev = "/dev/input/by-path/platform-i8042-serio-0-event-kbd";
-    fdi = open(dev, O_RDWR);
-    if (fdi == -1) {
-        fprintf(stderr, "Cannot open %s: %s.\n", dev, strerror(errno));
-        return EXIT_FAILURE;
-    }
-    struct input_event ev;
-    ssize_t n;
-    while (1) {
-        n = read(fdi, &ev, sizeof ev);
-        if (n == (ssize_t)-1) {
-            if (errno == EINTR)
-                continue;
-            else
-                break;
-        } else
-        if (n != sizeof ev) {
-            errno = EIO;
-            break;
-        }
-        if (ev.type == EV_KEY && ev.value >= 0 && ev.value <= 2){
-            printf("%s 0x%04x (%d)\n", evval[ev.value], (int)ev.code, (int)ev.code);
-            // ev.code = custom_keymap[ev.code];
-            if (evval[ev.value] == "PRESSED ")
-                send_new_event(fdo, ev.code);
-            }
+    rmp.remap("KEY_A", "KEY_H");
+    rmp.remap("KEY_S", "KEY_E");
+    rmp.remap("KEY_D", "KEY_L");
+    rmp.remap("KEY_F", "KEY_L");
+    rmp.remap("KEY_G", "KEY_O");
+    rmp.remap("KEY_H", "KEY_COMMA");
+    rmp.remap("KEY_J", "KEY_SPACE");
+    rmp.remap("KEY_Z", "KEY_M");
+    rmp.remap("KEY_X", "KEY_R");
+    rmp.remap("KEY_C", "KEY_DOT");
+    rmp.remap("KEY_V", "KEY_O");
+    rmp.remap("KEY_B", "KEY_L");
+    rmp.remap("KEY_N", "KEY_E");
+    rmp.remap("KEY_M", "KEY_H");
 
-        }
-    
-    fflush(stdout);
-    fprintf(stderr, "%s.\n", strerror(errno));
-   ioctl(fdo, UI_DEV_DESTROY);
-   close(fdo);
-   ioctl(fdi, UI_DEV_DESTROY);
-   close(fdi);
+    rmp.listen();
 
     return EXIT_FAILURE;
 }
--- Linux/remapper.hpp
+++ Linux/remapper.hpp
@@ -1,8 +1,12 @@
 #include <map>
 #include <linux/uinput.h>
 
-std::map<std::string, int> std_keymap =
+
+class Remapper
 {
+public:
+    std::map<std::string, int> std_keymap =
+            {
 // {"KEY_RESERVED",		KEY_RESERVED},
 // {"KEY_ESC",			KEY_ESC},
 // {"KEY_1",			KEY_1},
@@ -17,50 +21,50 @@ std::map<std::string, int> std_keymap =
 // {"KEY_0",			KEY_0},
 // {"KEY_MINUS",		KEY_MINUS},
 // {"KEY_EQUAL",		KEY_EQUAL},
-{"KEY_BACKSPACE",		KEY_BACKSPACE},
+                    {"KEY_BACKSPACE", KEY_BACKSPACE},
 // {"KEY_TAB",			KEY_TAB},
-{"KEY_Q",			KEY_Q},
-{"KEY_W",			KEY_W},
-{"KEY_E",			KEY_E},
-{"KEY_R",			KEY_R},
-{"KEY_T",			KEY_T},
-{"KEY_Y",			KEY_Y},
-{"KEY_U",			KEY_U},
-{"KEY_I",			KEY_I},
-{"KEY_O",			KEY_O},
-{"KEY_P",			KEY_P},
+                    {"KEY_Q",         KEY_Q},
+                    {"KEY_W",         KEY_W},
+                    {"KEY_E",         KEY_E},
+                    {"KEY_R",         KEY_R},
+                    {"KEY_T",         KEY_T},
+                    {"KEY_Y",         KEY_Y},
+                    {"KEY_U",         KEY_U},
+                    {"KEY_I",         KEY_I},
+                    {"KEY_O",         KEY_O},
+                    {"KEY_P",         KEY_P},
 // {"KEY_LEFTBRACE",		KEY_LEFTBRACE},
 // {"KEY_RIGHTBRACE",		KEY_RIGHTBRACE},
 // {"KEY_ENTER",		KEY_ENTER},
 // {"KEY_LEFTCTRL",		KEY_LEFTCTRL},
-{"KEY_A",			KEY_A},
-{"KEY_S",			KEY_S},
-{"KEY_D",			KEY_D},
-{"KEY_F",			KEY_F},
-{"KEY_G",			KEY_G},
-{"KEY_H",			KEY_H},
-{"KEY_J",			KEY_J},
-{"KEY_K",			KEY_K},
-{"KEY_L",			KEY_L},
+                    {"KEY_A",         KEY_A},
+                    {"KEY_S",         KEY_S},
+                    {"KEY_D",         KEY_D},
+                    {"KEY_F",         KEY_F},
+                    {"KEY_G",         KEY_G},
+                    {"KEY_H",         KEY_H},
+                    {"KEY_J",         KEY_J},
+                    {"KEY_K",         KEY_K},
+                    {"KEY_L",         KEY_L},
 // {"KEY_SEMICOLON",		KEY_SEMICOLON},
 // {"KEY_APOSTROPHE",		KEY_APOSTROPHE},
 // {"KEY_GRAVE",		KEY_GRAVE},
 // {"KEY_LEFTSHIFT",		KEY_LEFTSHIFT},
-{"KEY_BACKSLASH",		KEY_BACKSLASH},
-{"KEY_Z",			KEY_Z},
-{"KEY_X",			KEY_X},
-{"KEY_C",			KEY_C},
-{"KEY_V",			KEY_V},
-{"KEY_B",			KEY_B},
-{"KEY_N",			KEY_N},
-{"KEY_M",			KEY_M},
-{"KEY_COMMA",		KEY_COMMA},
-{"KEY_DOT",			KEY_DOT},
-{"KEY_SLASH",		KEY_SLASH},
+                    {"KEY_BACKSLASH", KEY_BACKSLASH},
+                    {"KEY_Z",         KEY_Z},
+                    {"KEY_X",         KEY_X},
+                    {"KEY_C",         KEY_C},
+                    {"KEY_V",         KEY_V},
+                    {"KEY_B",         KEY_B},
+                    {"KEY_N",         KEY_N},
+                    {"KEY_M",         KEY_M},
+                    {"KEY_COMMA",     KEY_COMMA},
+                    {"KEY_DOT",       KEY_DOT},
+                    {"KEY_SLASH",     KEY_SLASH},
 // {"KEY_RIGHTSHIFT",		KEY_RIGHTSHIFT},
 // {"KEY_KPASTERISK",		KEY_KPASTERISK},
 // {"KEY_LEFTALT",		KEY_LEFTALT},
-{"KEY_SPACE",		KEY_SPACE},
+                    {"KEY_SPACE",     KEY_SPACE},
 // {"KEY_CAPSLOCK",		KEY_CAPSLOCK},
 // {"KEY_F1",			KEY_F1},
 // {"KEY_F2",			KEY_F2},
@@ -251,6 +255,155 @@ std::map<std::string, int> std_keymap =
 // {"KEY_WIMAX",		KEY_WIMAX},
 // {"KEY_RFKILL",		KEY_RFKILL},
 // {"KEY_MICMUTE",		KEY_MICMUTE}
-};
+            };
 
-std::map<int, int> custom_keymap;
+    std::map<int, int> custom_keymap;
+    int fdi;
+    int fdo = open("/dev/uinput", O_WRONLY | O_NONBLOCK);
+    struct input_event ev;
+    ssize_t n;
+
+
+    void init_custom_keymap()
+    {
+        for (auto &p : std_keymap)
+            custom_keymap[p.second] = p.second;
+    }
+
+
+    const char *const evval[3] = {
+            "RELEASED",
+            "PRESSED ",
+            "REPEATED"
+    };
+
+
+    Remapper()
+    {
+        init_custom_keymap();
+        struct uinput_setup usetup;
+
+//    /*
+//     * The ioctls below will enable the device that is about to be
+//     * created, to pass key events, in this case the space key.
+//     */
+        ioctl(fdo, UI_SET_EVBIT, EV_KEY);
+        for (auto &p: std_keymap)
+            //    std::cout << p.second << std::endl;
+            ioctl(fdo, UI_SET_KEYBIT, p.second);
+
+        memset(&usetup, 0, sizeof(usetup));
+        usetup.id.bustype = BUS_USB;
+        usetup.id.vendor = 0x1234; /* sample vendor */
+        usetup.id.product = 0x5678; /* sample product */
+        strcpy(usetup.name, "Example device");
+
+        ioctl(fdo, UI_DEV_SETUP, &usetup);
+        ioctl(fdo, UI_DEV_CREATE);
+        /*
+         * On UI_DEV_CREATE the kernel will create the device node for this
+         * device. We are inserting a pause here so that userspace has time
+         * to detect, initialize the new device, and can start listening to
+         * the event, otherwise it will not notice the event we are about
+         * to send. This pause is only needed in our example code!
+         */
+        const char *dev = "/dev/input/by-path/platform-i8042-serio-0-event-kbd";
+        fdi = open(dev, O_RDWR);
+        if (fdi == -1)
+        {
+            fprintf(stderr, "Cannot open %s: %s.\n", dev, strerror(errno));
+            // return EXIT_FAILURE;
+        }
+    }
+
+
+    ~Remapper()
+    {
+        fflush(stdout);
+        fprintf(stderr, "%s.\n", strerror(errno));
+        ioctl(fdo, UI_DEV_DESTROY);
+        close(fdo);
+        ioctl(fdi, UI_DEV_DESTROY);
+        close(fdi);
+    }
+
+
+    void listen()
+    {
+        while (1)
+        {
+            n = read(fdi, &ev, sizeof ev);
+            if (n == (ssize_t) -1)
+            {
+                if (errno == EINTR)
+                    continue;
+                else
+                    break;
+            }
+            else if (n != sizeof ev)
+            {
+                errno = EIO;
+                break;
+            }
+            if (ev.type == EV_KEY && ev.value >= 0 && ev.value <= 2)
+            {
+                printf("%s 0x%04x (%d)\n", evval[ev.value], (int) ev.code, (int) ev.code);
+                // ev.code = custom_keymap[ev.code];
+                if (evval[ev.value] == "PRESSED ")
+                    send_new_event(fdo, ev.code);
+            }
+
+        }
+    }
+
+
+    void emit(int fd, int type, int code, int val)
+    {
+        struct input_event ie;
+
+        ie.type = type;
+        ie.code = code;
+        ie.value = val;
+        /* timestamp values below are ignored */
+        ie.time.tv_sec = 0;
+        ie.time.tv_usec = 0;
+        // std::cout << "sending: " << code << std::endl
+        write(fd, &ie, sizeof(ie));
+    }
+
+
+    void send_new_event(int fdo, int code)
+    {
+        if (code != custom_keymap[code])
+        {
+            std::cout << "from: " << code << " to: " << custom_keymap[code] << std::endl;
+            // sleep(1);
+            emit(fdo, EV_KEY, KEY_BACKSPACE, 1);
+            emit(fdo, EV_SYN, SYN_REPORT, 0);
+            emit(fdo, EV_KEY, KEY_BACKSPACE, 0);
+            emit(fdo, EV_SYN, SYN_REPORT, 0);
+            // sleep(1);
+            emit(fdo, EV_KEY, custom_keymap[code], 1);
+            emit(fdo, EV_SYN, SYN_REPORT, 0);
+            emit(fdo, EV_KEY, custom_keymap[code], 0);
+            emit(fdo, EV_SYN, SYN_REPORT, 0);
+            // sleep(1);
+        }
+        else
+        {
+            std::cout << "from: " << code << " to: " << code << std::endl;
+            emit(fdo, EV_KEY, code, 1);
+            emit(fdo, EV_SYN, SYN_REPORT, 0);
+            emit(fdo, EV_KEY, code, 0);
+            emit(fdo, EV_SYN, SYN_REPORT, 0);
+        }
+    }
+
+
+    inline void remap(const std::string &original, const std::string &remapped)
+    {
+        custom_keymap[std_keymap[original]] = std_keymap[remapped];
+    }
+
+
+};
